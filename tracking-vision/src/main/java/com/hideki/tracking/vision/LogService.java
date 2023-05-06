bd7bd35792cfa8870dfc5db590d427c24742de4f
pauloAlmeida01 <paulo.rsantos@sptech.school>
2023-05-06 14:04:33 -0300

    Build jar

--- tracking-vision/src/main/java/com/hideki/tracking/vision/LogService.java
+++ tracking-vision/src/main/java/com/hideki/tracking/vision/LogService.java
@@ -18,7 +18,7 @@ public class LogService {
         JdbcTemplate con = conexao.getConnection();
         JdbcTemplate conMysql = conexaoMysql.getConnection();
 
-        
+        //quaz
         con.update("insert into log(horarioCapturado,janelaPid,tituloJanela,usoCpu,usoDisco,usoRam,bytesRecebidos,bytesEnviados,fkMaquina) values (?, ?, ?, ?, ?,?,?,?,?)", log.getHorarioCapturado(), log.getJanelaPid(), log.getTituloJanela(), log.getUsoCpu(), log.getUsoDisco(), log.getUsoRam(),log.getBytesRecebidos(),log.getBytesEnviados(), log.getFkMaquina());
 
 //        conMysql.update("insert into log(idLog,horarioCapturado,janelaPid,tituloJanela,usoCpu,usoDisco,usoRam,bytesRecebidos,bytesEnviados,fkMaquina) values (?,?, ?, ?, ?, ?,?,?,?,?)",retornarIdLog(log), log.getHorarioCapturado(), log.getJanelaPid(), log.getTituloJanela(), log.getUsoCpu(), log.getUsoDisco(), log.getUsoRam(),log.getBytesRecebidos(),log.getBytesEnviados(), log.getFkMaquina());
