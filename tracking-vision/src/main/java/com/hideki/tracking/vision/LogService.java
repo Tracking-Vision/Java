e4b9241f24b7d60ddd9ab22b6767a479c4cd74d3
pauloAlmeida01 <paulo.rsantos@sptech.school>
2023-05-06 13:16:45 -0300

    Teste para banco mysql no container

--- tracking-vision/src/main/java/com/hideki/tracking/vision/LogService.java
+++ tracking-vision/src/main/java/com/hideki/tracking/vision/LogService.java
@@ -12,7 +12,15 @@ public class LogService {
 
 
         con.update("insert into log(horarioCapturado,janelaPid,tituloJanela,usoCpu,usoDisco,usoRam,bytesRecebidos,bytesEnviados,fkMaquina) values (?, ?, ?, ?, ?,?,?,?,?)", log.getHorarioCapturado(), log.getJanelaPid(), log.getTituloJanela(), log.getUsoCpu(), log.getUsoDisco(), log.getUsoRam(),log.getBytesRecebidos(),log.getBytesEnviados(), log.getFkMaquina());
-        conMysql.update("insert into log(horarioCapturado,janelaPid,tituloJanela,usoCpu,usoDisco,usoRam,bytesRecebidos,bytesEnviados,fkMaquina) values (?, ?, ?, ?, ?,?,?,?,?)", log.getHorarioCapturado(), log.getJanelaPid(), log.getTituloJanela(), log.getUsoCpu(), log.getUsoDisco(), log.getUsoRam(),log.getBytesRecebidos(),log.getBytesEnviados(), log.getFkMaquina());
+        conMysql.update("insert into log(idLog,horarioCapturado,janelaPid,tituloJanela,usoCpu,usoDisco,usoRam,bytesRecebidos,bytesEnviados,fkMaquina) values (?,?, ?, ?, ?, ?,?,?,?,?)",retornarIdLog(log), log.getHorarioCapturado(), log.getJanelaPid(), log.getTituloJanela(), log.getUsoCpu(), log.getUsoDisco(), log.getUsoRam(),log.getBytesRecebidos(),log.getBytesEnviados(), log.getFkMaquina());
+    }
+
+    public Integer retornarIdLog (Log log) {
+        Conexao conexao = new Conexao();
+
+        JdbcTemplate con = conexao.getConnection();
+
+        return con.queryForObject("select idLog from log where horarioCapturado = ? and janelaPid = ? and tituloJanela = ? and usoCpu = ? and usoDisco = ? and usoRam = ? and bytesRecebidos = ? and bytesEnviados = ? and fkMaquina = ?", Integer.class, log.getHorarioCapturado(), log.getJanelaPid(), log.getTituloJanela(), log.getUsoCpu(), log.getUsoDisco(), log.getUsoRam(),log.getBytesRecebidos(),log.getBytesEnviados(), log.getFkMaquina());
     }
 
 }
